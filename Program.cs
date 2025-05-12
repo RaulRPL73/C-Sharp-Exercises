7c51a915d79b1f466a4d112b7a61c19898efbcdd
RaulRPL73 <raulrpl733@gmail.com>
2025-05-12 13:14:56 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 2,1,2,3,3,4,2,4};
+        bumeral(numbers);
+    }
+
+    static void bumeral(int[] numbers)
+    {
+        int index=0;
+        foreach(int value in numbers)
+        {
+            if (index>0 && index<numbers.Length-1)
+            {
+                if(numbers[index-1]==numbers[index+1])
+                {
+                    Console.WriteLine("The numbers: "+numbers[index-1]+" "+value+" "+numbers[index+1]+" are bumeral");
+                }
+            }
+            index++;
+        }
+    }
+    
+}
