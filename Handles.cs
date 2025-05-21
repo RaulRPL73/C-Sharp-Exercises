24a457de2c86eed0d4b1abe485def4b0a28fd14a
RaulRPL73 <raulrpl733@gmail.com>
2025-05-21 13:12:21 -0700

    Add files via upload

---
+++ Handles.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string word = "Hello @user, it's good to see you, you are the number #12 in seeing this on https://github.com/RaulRPL73";
+        handles(word);
+    }
+
+    public static void handles(string word)
+    {
+        Regex userRegex = new Regex(@"@\w+");
+        Regex hashtagRegex = new Regex(@"#\w+");
+        Regex webRegex = new Regex(@"(https?://[^\s]+)");
+
+        List<string> arroba = new List<string>();
+        List<string> hashtags = new List<string>();
+        List<string> webs = new List<string>();
+
+        foreach (Match match in userRegex.Matches(word))
+        {
+            arroba.Add(match.Value);
+        }
+
+        foreach (Match match in hashtagRegex.Matches(word))
+        {
+            hashtags.Add(match.Value);
+        }
+
+        foreach (Match match in webRegex.Matches(word))
+        {
+            webs.Add(match.Value);
+        }
+
+        Console.WriteLine("Arroba detected: " + string.Join(", ", arroba));
+        Console.WriteLine("Hashtags detected: " + string.Join(", ", hashtags));
+        Console.WriteLine("Web detected: " + string.Join(", ", webs));
+    }
+}
