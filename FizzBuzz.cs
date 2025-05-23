35dd4ccdc30c094bbb0c9c03ef4c95a1e76d45ff
RaulRPL73 <raulrpl733@gmail.com>
2025-05-23 12:21:19 -0700

    Add files via upload

---
+++ FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿//This code was made on the logic of fizz and buzz
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int number = 0;
+        FizzBuzz(number);
+    }
+
+    public static void FizzBuzz(int number)
+    {
+        for (number = 1; number <= 100; number++)
+        {
+            //FizzBuzz because both numbers are devided between 5 and 3 and the result is 0 
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine(number + " fizzbuzz");
+            }
+            //Fizz because the number devided by 3 equals 0
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine(number + " fizz");
+            }
+            //Buzz because the number devided by 5 equals 0
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine(number + " buzz");
+            }
+        }
+    }
+}
