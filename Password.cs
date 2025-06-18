bbd779948049bf5d382e82b8384927763f3456e5
RaulRPL73 <raulrpl733@gmail.com>
2025-06-18 11:49:07 -0700

    Add files via upload

---
+++ Password.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int lenght = 12;
+        string[] password = new string[lenght];
+        generator(password);
+    }
+
+    //Generates the password
+    public static void generator(string[] pass)
+    {
+        //Selects a random letter or number or special case to add to your password
+        const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+        Random random = new Random();
+
+        for (int i = 0; i < pass.Length; i++)
+        {
+            char randomChar = characters[random.Next(characters.Length)];
+            pass[i] = randomChar.ToString();
+        }
+        Console.WriteLine("Your password is: " + string.Join("", pass));
+    }
+}
