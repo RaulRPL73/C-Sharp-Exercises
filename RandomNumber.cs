604afc36127ac17fbc54ff394e70208274f49326
RaulRPL73 <raulrpl733@gmail.com>
2025-06-26 12:09:27 -0700

    Add files via upload

---
+++ RandomNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//This program generates a number between 0 and 100 without using random as a method
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int number = 0;
+        number = generator(number);
+        Console.WriteLine($"The number generated is: {number}");
+    }
+
+    public static int generator(int number)
+    {
+        int minimum = 0, maximum = 100;
+        /*This line grabs the current date and time from the system and retrieves its tick count and becuase it detects
+        how many ticks can pass between now and midniight we use long to support it*/
+        long ticks = DateTime.Now.Ticks;
+        int n = (int)(ticks % (maximum - minimum + 1)) + minimum;
+        return n;
+    }
+}
